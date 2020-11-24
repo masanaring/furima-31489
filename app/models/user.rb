a76fc68005d42396380d3c9097c99863fa01611a
masanaring <73056512+masanaring@users.noreply.github.com>
2020-11-25 03:59:49 +0900

    ユーザー管理機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         validates :nickname,             presence: true
+         validates :encrypted_password,   presence: true
+         validates :first_name,           presence: true
+         validates :family_name,          presence: true
+         validates :first_name_furigana,  presence: true
+         validates :family_name_furigana, presence: true
+         validates :birthday,             presence: true
+end
+
