6eaff86d43b2205cf3a07756bcdda68ea39e2d91
masanaring <73056512+masanaring@users.noreply.github.com>
2020-12-13 21:54:32 +0900

    コメント機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         has_many :comments
+         has_many :items
+
   with_options presence: true do
     validates :nickname
     validates :birthday
